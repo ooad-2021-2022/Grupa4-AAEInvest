e2dc8e2c773bef771ab4b007fc95ff1bd1ced074
Almina Brulić <92340302+abrulic1@users.noreply.github.com>
2022-06-15 03:13:42 +0200

    Validacija datuma

--- OOAD2022/Models/Rezervacija.cs
+++ OOAD2022/Models/Rezervacija.cs
@@ -4,14 +4,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OOAD2022.Models
 {
+    public class ValidateDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid
+        (object date, ValidationContext validationContext)
+        {
+            return ((DateTime)date >= DateTime.Now)
+            ? ValidationResult.Success
+            : new ValidationResult("Validan je datum od datuma danasnjeg dana");
+        }
+    }
     public class Rezervacija
     {
         [Key]
         public int Id { get; set; }
         public int Sifra { get; set; }
         [Display(Name = "Datum dolaska")]
+        [ValidateDate]
+        [DataType(DataType.Date)]
         public DateTime DatumDolaska { get; set; }
         [Display(Name = "Datum odlaska")]
+        [ValidateDate]
+        [DataType(DataType.Date)]
         public DateTime DatumOdlaska { get; set; }
         public int Cijena { get; set; }
 
